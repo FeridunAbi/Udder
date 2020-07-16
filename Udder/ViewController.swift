2ea2b1c1c30f9d9aa84ffe1209d5a5f8f8a3fe53
FeridunAbi <ramazanusen@hotmail.com>
2020-07-16 23:26:59 +0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Ramazan Üsen on 16.07.2020.
+//  Copyright © 2020 Ramazan Üsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
